9573e95cb92b21b4e83c9ec0315474ece50ba5bd
NanairoNika <nikschenn@gmail.com>
2016-10-17 00:23:13 +0300

    Python1

---
+++ Homework3/firstpythonhw.py
@@ -0,0 +1,13 @@
+a=int(input('Enter a'))
+b=int(input('Enter b'))
+c=int(input('Enter c'))
+
+if a * b == c :
+	print('Произведение a и b равно с')
+else :
+	print('Произведение a и b не равно с')
+if a * c + b == 0:
+	print('c является решением линейного уравнения ax + b = 0')
+else:
+	print('c не является решением линейного уравнения ax + b = 0')
+
