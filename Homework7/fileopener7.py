892e26103e6229574ce815a1dec1139195d0b34e
NanairoNika <nikschenn@gmail.com>
2016-12-06 01:19:20 +0300

    Update fileopener7.py

--- Homework7/fileopener7.py
+++ Homework7/fileopener7.py
@@ -3,12 +3,9 @@ b=0
 m=0
 f = open('text.txt','r', encoding = 'utf-8')
 for line in f:
-    print (line)
-    print (len(line))
     if len(line)-1 > b or b==0:
         b = len(line)-1
     if len(line)-1 < m or m==0:
         m = len(line)-1
-print (b,m)
 a = b/m
 print (a)
