5d1886c533eb57cb9280bd7a25a8cf5cc029bacd
NanairoNika <nikschenn@gmail.com>
2016-12-06 00:00:26 +0300

    Update fileopener7.py

--- Homework7/fileopener7.py
+++ Homework7/fileopener7.py
@@ -1,14 +1,14 @@
 tekst=[]
 with open('text.txt','r', encoding = 'utf-8') as f:
-    smth = f.read()
-    smth = smth.replace('\n',' ')
-    tekst = smth.split(' ') 
-m=len(tekst[0])
-b=len(tekst[0])
+    for line in f:
+        tekst = f.readlines()
+m=len(tekst[0]-1)
+b=len(tekst[0]-1)
 for i in tekst:
-    if len(i) > b:
-        b = len(i)
-    if len(i) < m:
-        m = len(i)
+    if len(i)-1 > b:
+        b = len(i)-1
+    if len(i)-1 < m:
+        m = len(i)-1
+print (b,m)
 a = b/m
 print (a)
