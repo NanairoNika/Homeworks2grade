367b5a1e4e9c1342855e3eabe6977f849ba8f8dc
NanairoNika <nikschenn@gmail.com>
2017-01-23 21:44:39 +0300

    Update Hw11.py

--- Homework11/Hw11.py
+++ Homework11/Hw11.py
@@ -1,16 +1,19 @@
 import random
-colloc = {'мобильный' :'телефон',
-          'тусклая' :'лампочка',
-          'заходящее' :'солнце',
-          'воздушный' :'шарик',
-          'тюремная' :'камера',
-          'кадетский' :'корпус'}
-adjlist = list(colloc.keys())
-word = random.choice(adjlist)
-print (word)
-gword = input("Какое слово загадано? ")
-gword = gword.lower()
-if gword == colloc[word]:
-    print ("Хорошая работа! Ты прав!")
-else:
-    print ("Нет(. Попробуй в другой раз.")
+
+def main():
+    colloc = {'мобильный' :'телефон',
+              'тусклая' :'лампочка',
+              'заходящее' :'солнце',
+              'воздушный' :'шарик',
+              'тюремная' :'камера',
+              'кадетский' :'корпус'}
+    adjlist = list(colloc.keys())
+    word = random.choice(adjlist)
+    print (word)
+    gword = input("Какое слово загадано? ")
+    gword = gword.lower()
+    if gword == colloc[word]:
+        print ("Хорошая работа! Ты прав!")
+    else:
+        print ("Нет(. Попробуй в другой раз.")
+main()
