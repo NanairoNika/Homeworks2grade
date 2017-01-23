02727dabcc5dad64af6d57dd4d745b227d6b14fc
NanairoNika <nikschenn@gmail.com>
2017-01-23 21:38:27 +0300

    Add files via upload

---
+++ Homework11/Hw11.py
@@ -0,0 +1,16 @@
+import random
+colloc = {'мобильный' :'телефон',
+          'тусклая' :'лампочка',
+          'заходящее' :'солнце',
+          'воздушный' :'шарик',
+          'тюремная' :'камера',
+          'кадетский' :'корпус'}
+adjlist = list(colloc.keys())
+word = random.choice(adjlist)
+print (word)
+gword = input("Какое слово загадано? ")
+gword = gword.lower()
+if gword == colloc[word]:
+    print ("Хорошая работа! Ты прав!")
+else:
+    print ("Нет(. Попробуй в другой раз.")
