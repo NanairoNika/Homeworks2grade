6967b5be23b76fe5bcf759b332f913a38d42891c
NanairoNika <nikschenn@gmail.com>
2017-01-31 00:01:10 +0300

    Add files via upload

---
+++ HW12/111111111.py
@@ -0,0 +1,22 @@
+import re
+def textcleaner(cleanedtxt):
+    f = open ('dost1.txt','r', encoding = 'utf-8')
+    text = f.read()
+    text = text.lower()
+    text = text.split()
+    for word in text:
+        word = word.strip('—!;.,?’:"…()•]«»-')
+        cleanedtxt.append(word)
+    for word in cleanedtxt:
+        if word == '':
+            cleanedtxt.remove(word)
+    f.close()
+    return cleanedtxt
+text = []
+findfind = []
+text = textcleaner(text)
+for word in text:
+    iigood = re.search('на((й(т|д)(и|у|ё)(т?|шь|м)?е?)|(хо(жу|д(и|я)(м|шь|те|т)?))|(шё?л(и|а)?))', word)
+    if iigood:
+        findfind.append(word)
+print(findfind)
