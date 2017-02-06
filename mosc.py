079f8f1faf7ea5de47ea4c904612ded3f6d09b3f
NanairoNika <nikschenn@gmail.com>
2017-02-06 16:15:55 +0300

    Add files via upload

--- mosc.py
+++ mosc.py
@@ -1,6 +1,16 @@
 import re
 f = open ('cons.html' , 'r', encoding = 'utf-8')
 text = f.read()
-UTC = re.findall('UTC\+[1234567890]', text)
-
-
+UTC = re.findall('UTC\+[1234567890]+', text)
+consttime = [UTC[0]]
+counter = 0
+def WintSummTime
+    for time in UTC:
+        for swtime in consttime:
+            if time == swtime:
+                counter += 1
+        if counter == 0:
+            consttime.append(time)
+        counter = 0
+print ('Зимнее время >', consttime[0])
+print ('Летнее время >', consttime[1])
