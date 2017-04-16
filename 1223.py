0e42e516ab8607b026bf1e0c1ad8d69f791ec9b8
NanairoNika <nikschenn@gmail.com>
2017-04-16 19:31:53 +0300

    Add files via upload

---
+++ 1223.py
@@ -0,0 +1,17 @@
+import re
+import os
+
+
+def Seeker():
+    files = os.listdir()
+    nofold = []
+    for file in files:
+        if os.path.isfile(file):
+            nofold.append(file)
+    return nofold
+
+
+def 
+text = []
+text = Seeker()
+print (text)
